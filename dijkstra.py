cb62aaf30516208cc90cfe573bbd5a33169a7e72
rashad <mkrashad25@gmail.com>
2022-08-26 17:04:43 +0200

    Implement text conversion in dijkstra algorithm

--- dijkstra.py
+++ dijkstra.py
@@ -1,34 +1,15 @@
-graph = [[0, 4, 0, 0, 0, 0, 0, 8, 0],
-         [4, 0, 8, 0, 0, 0, 0, 11, 0],
-         [0, 8, 0, 7, 0, 4, 0, 0, 2],
-         [0, 0, 7, 0, 9, 14, 0, 0, 0],
-         [0, 0, 0, 9, 0, 10, 0, 0, 0],
-         [0, 0, 4, 14, 10, 0, 2, 0, 0],
-         [0, 0, 0, 0, 0, 2, 0, 1, 6],
-         [8, 11, 0, 0, 0, 0, 1, 0, 7],
-         [0, 0, 2, 0, 0, 0, 6, 7, 0]
-         ]
+import timeit
+
+# Global variables
 infinity = 999999
 
 
-def dijkstra(graph, source):
-    length = len(graph)
-    visitedVertex = [bool(i) for i in range(length)]
-    distance = [int(i) for i in range(length)]
-    for i in range(length):
-        visitedVertex[i] = False
-        distance[i] = infinity
-
-    distance[source] = 0
-    for i in range(length):
-        u = findMinDistance(distance, visitedVertex)
-        visitedVertex[u] = True
-        for v in range(length):
-            if visitedVertex[v] is False and graph[u][v] != 0 and distance[u] + graph[u][v] < distance[v]:
-                distance[v] = distance[u] + graph[u][v]
-
-    for i in range(len(distance)):
-        print(f'Distance from, {source}, {i}, {distance[i]}')
+def convertTextToArray():
+    graph = []
+    with open('examples/graph-100.txt', 'r') as file:
+        for line in file:
+            graph.append([int(i) for i in line.split()])
+    return graph
 
 
 def findMinDistance(distance, visitedVertex):
@@ -41,4 +22,36 @@ def findMinDistance(distance, visitedVertex):
     return minDistanceVertex
 
 
-dijkstra(graph, 0)
+def dijkstra(graph, source):
+    out_file = open('results/dijkstra-result-seq.txt', 'w')
+    graphLength = len(graph)
+    visitedVertex = [bool(i) for i in range(graphLength)]
+    distance = [int(i) for i in range(graphLength)]
+    for i in range(graphLength):
+        visitedVertex[i] = False
+        distance[i] = infinity
+
+    distance[source] = 0
+    for i in range(graphLength):
+        u = findMinDistance(distance, visitedVertex)
+        visitedVertex[u] = True
+        for v in range(graphLength):
+            if visitedVertex[v] is False and graph[u][v] != 0 and distance[u] + graph[u][v] < distance[v]:
+                distance[v] = distance[u] + graph[u][v]
+    for i in range(len(distance)):
+        print(
+            f'Distance from source {source} to node {i} equals {distance[i]}', file=out_file)
+    print(
+        f'Distance from source {source} to destination node {i} equals {distance[graphLength-1]}')
+
+
+def main():
+    matrix = convertTextToArray()
+    start_time = timeit.default_timer()
+    dijkstra(matrix, 0)
+    stop_time = timeit.default_timer()
+    print(f'Total time takes: {stop_time - start_time}')
+
+
+if __name__ == "__main__":
+    main()
